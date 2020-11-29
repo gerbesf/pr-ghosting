3776cf78ade808faae7b55ae786440c14cf3d2f7
Gerbes Matos <gerbes.matos@gmail.com>
2020-11-29 17:59:30 -0300

    dddos

--- resources/views/blocks/mapstatus.blade.php
+++ resources/views/blocks/mapstatus.blade.php
@@ -10,7 +10,7 @@
         <span class="badge badge-light">Empty</span>
     @endif
     @if($session->status == 'inative')
-        <span class="badge badge-light">Inativo</span>
+        <span class="badge badge-light">Inative</span>
     @endif
     @if($session->status == 'offline')
         <span class="badge badge-danger">Offline</span>
